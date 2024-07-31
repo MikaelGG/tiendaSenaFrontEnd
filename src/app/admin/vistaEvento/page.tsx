fc9ce712212850501e65df3d7c430c17c9e4ac84
MIKAELGG <mikael2g3g@gmail.com>
2024-07-31 14:54:02 -0500

    Update date

--- src/app/admin/vistaEvento/page.tsx
+++ src/app/admin/vistaEvento/page.tsx
@@ -46,10 +46,9 @@ export default function Eventos(){
 
     function formatDate(dateString: any) {
         const date = new Date(dateString);
-        const year = date.getFullYear();
-        const month = (date.getMonth() + 1).toString().padStart(2, '0');
-        const day = (date.getDate() + 1).toString().padStart(2, '0');
-        return `${year}-${month}-${day}`;
+        const formatter = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' });
+        const formattedDate = formatter.format(date);
+        return formattedDate;
     }
 
     return(<>
